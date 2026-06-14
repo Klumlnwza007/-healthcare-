621fec1d9201bcd2981a5ca2c227fdcc6822b8d8
Klumlnwza007 <mulk.sekr@gmail.com>
2026-06-15 06:16:45 +0700

    setting draft 1

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const loginPage = document.querySelector('.log_in');
 const chatbotPage = document.querySelector('.chatbot');
 const overviewPage = document.querySelector('.overview');
+const couponPage = document.querySelector('.coupon');
 
 const headChatbot = document.querySelector('.header-chatbot');
 const headOverview = document.querySelector('.header-overview');
+const headCoupon = document.querySelector('.header-coupon');
 
 loginPage.style.display = 'none';
 chatbotPage.style.display = 'none';
 overviewPage.style.display = 'none';
+couponPage.style.display = 'none';
 
 headChatbot.style.display = 'none';
 headOverview.style.display = 'none';
+headCoupon.style.display = 'none';
