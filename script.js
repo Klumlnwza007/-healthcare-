56811a6c99c134f3c6221e789db931416f8a5c47
Klumlnwza007 <mulk.sekr@gmail.com>
2026-06-14 16:42:27 +0700

    overview page draft 1

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const loginPage = document.querySelector('.log_in');
+const chatbotPage = document.querySelector('.chatbot');
+const headChatbot = document.querySelector('.header-chatbot');
 
 loginPage.style.display = 'none';
+chatbotPage.style.display = 'none';
+headChatbot.style.display = 'none';
