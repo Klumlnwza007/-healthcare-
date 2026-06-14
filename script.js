2c805706200732115407a4852c7433ca43feb752
Klumlnwza007 <mulk.sekr@gmail.com>
2026-06-14 20:12:36 +0700

    coupon draft 1

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const loginPage = document.querySelector('.log_in');
 const chatbotPage = document.querySelector('.chatbot');
+const overviewPage = document.querySelector('.overview');
+
 const headChatbot = document.querySelector('.header-chatbot');
+const headOverview = document.querySelector('.header-overview');
 
 loginPage.style.display = 'none';
 chatbotPage.style.display = 'none';
+overviewPage.style.display = 'none';
+
 headChatbot.style.display = 'none';
+headOverview.style.display = 'none';
